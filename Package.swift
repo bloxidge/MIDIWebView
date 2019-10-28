ae1c327fa3ec7174912fc1081925cf7f9943f452
Peter Bloxidge <peter.bloxidge@music-group.com>
2019-10-28 00:48:39 +0000

    Fix package path and minimum platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MIDIWebView",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://peter.bloxidge@bitbucket.innouk.music-group.com/scm/~peter.bloxidge/webmidikit.git", .branch("master")),
+        .package(url: "ssh://git@bitbucket.innouk.music-group.com:2222/~peter.bloxidge/webmidikit.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MIDIWebView",
-            dependencies: []),
+            dependencies: ["WebMIDIKit"]),
         .testTarget(
             name: "MIDIWebViewTests",
             dependencies: ["MIDIWebView"]),
